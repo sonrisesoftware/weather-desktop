6c62f8630233e8f5581d2ecd356d616042f74f45
Michael Spencer <spencers1993@gmail.com>
2013-05-11 16:29:52 -0500

    Initial implementation of Location

--- src/weather/location.h
+++ src/weather/location.h
@@ -20,13 +20,17 @@
 #ifndef LOCATION_H
 #define LOCATION_H
 
+#include "main.h"
 #include "cache.h"
 
 #include <QObject>
+#include <QString>
 #include <QVariantMap>
+#include <QTime>
 
 namespace Weather
 {
+
 	class Conditions;
 	class Service;
 
@@ -40,22 +44,73 @@ namespace Weather
 		
 		Q_PROPERTY(Weather::Service *service READ service WRITE setService NOTIFY serviceChanged)
 		Q_PROPERTY(QVariantMap data READ data WRITE setData NOTIFY dataChanged)
-		Q_PROPERTY(Cache *cache READ cache NOTIFY cacheChanged)
 		
 		Q_PROPERTY(Weather::Conditions *conditions READ conditions NOTIFY conditionsChanged)
 		// TODO: Add more weather types here
 		
+		Q_PROPERTY(bool needsRefresh READ needsRefresh NOTIFY needsRefreshChanged)
 		Q_PROPERTY(bool refreshing READ isRefreshing NOTIFY refreshingChanged)
 		Q_PROPERTY(bool error READ hasError NOTIFY errorChanged)
 		Q_PROPERTY(QString errorString READ errorString NOTIFY errorStringChanged)
 		
 		Q_PROPERTY(bool day READ isDay NOTIFY dayChanged)
 		
-		Q_PROPERTY(QString lastUpdated READ lastUpdated NOTIFY lastUpdatedChanged)
+		Q_PROPERTY(QTime lastUpdated READ lastUpdated NOTIFY lastUpdatedChanged)
+		
+		STATIC_PROPERTY(bool, autoRefresh, autoRefresh, setAutoRefresh)
+		STATIC_PROPERTY(int, refreshTime, refreshTime, setRefreshTime)
+		STATIC_PROPERTY(Weather::Service *, defaultService, defaultService, setDefaultService)
 			
 	public:
 		explicit Location(const QString& name, const QString& location, Service *service, QObject *parent = 0);
-	
+		explicit Location(const QString& name, const QString& location, QObject *parent = 0);
+		
+		virtual ~Location();
+		
+		/**
+		 * Called from Service when new weather data is completly downloaded.
+		 */
+		void finishRefresh(QVariantMap data, QString error);
+		
+		static Cache *cache() {
+			if (s_cache == nullptr) {
+				s_cache = new Cache(KStandardDirs::locateLocal("appdata", "weather-cache"));
+			}
+			
+			return s_cache;
+		}
+		
+		static void setCache(Cache *cache) {
+			Q_ASSERT(s_cache == nullptr);
+			
+			s_cache = cache;
+		}
+		
+		static void setCache(const QString& cacheDirectory) {
+			setCache(new Cache(cacheDirectory));
+		}
+		
+	private:
+		static Cache *s_cache;
+		
+	public slots:
+		void refresh();
+		void cancelRefresh();
+		
+	private slots:
+		/*
+		 * Called by the timer when it is time to refresh again.
+		 */
+		void timeToRefresh();
+		
+		/**
+		 * Called when the location changed to handle refreshing.
+		 */
+		void onLocationChanged();
+		
+	signals:
+		void refreshed();
+
 	#include "weather/location.gen"
 	};
 
